c0b3fd805892f8fe3997f3ef026d655aae890add
Melissa Zhang <melz63017@gmail.com>
2021-03-20 21:21:52 -0700

    fix images

--- app/hooks/useRewardOptions.ts
+++ app/hooks/useRewardOptions.ts
@@ -1,8 +1,29 @@
 import { RewardOption } from "../types";
 export default function useEarnInfo(): Array<RewardOption> {
   return [
-    { type: "generic", title: "Ask me anything!", description: "Ask me literally anything, and I’ll do my best to answer.", cost: 1200,  currency: "$MATT", image: "http://localhost:3000/images/ama.png"},
-    { type: "nft", title: "Genesis NFT", description: "NFT description goes here... lorem ipsum", cost: 2500,  currency: "$MATT", image: "http://localhost:3000/images/nft.png"},
-    { type: "nft", title: "Other thing", description: "Lorem ipsum lorem ipsum lorem ipsum", cost: 1242,  currency: "$MATT", image: "http://localhost:3000/images/other.png"},
+    {
+      type: "generic",
+      title: "Ask me anything!",
+      description: "Ask me literally anything, and I’ll do my best to answer.",
+      cost: 1200,
+      currency: "$MATT",
+      image: "/images/ama.png",
+    },
+    {
+      type: "nft",
+      title: "Genesis NFT",
+      description: "NFT description goes here... lorem ipsum",
+      cost: 2500,
+      currency: "$MATT",
+      image: "/images/nft.png",
+    },
+    {
+      type: "nft",
+      title: "Other thing",
+      description: "Lorem ipsum lorem ipsum lorem ipsum",
+      cost: 1242,
+      currency: "$MATT",
+      image: "/images/other.png",
+    },
   ];
 }
