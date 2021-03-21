04cb12cedeedbe1d025a20c82dac3e74234c2fc7
Melissa Zhang <melz63017@gmail.com>
2021-03-20 18:06:05 -0700

    Remove console logs

--- app/pages/api/invite.ts
+++ app/pages/api/invite.ts
@@ -7,20 +7,16 @@ export default async function handler(req, res) {
     return res.status(200).json({ redirect_url: `/` });
   }
   const code = params.code;
-  console.log(params.code);
   if (req.method === "GET") {
     const codes = await db("ReferralCode")
       .select({
         filterByFormula: `referral_code="${code}"`,
       })
       .all();
-    console.log("codes", codes);
     if (codes.length === 0) {
-      console.log("redirect");
       return res.status(200).json({ redirect_url: `/` });
     }
     const creatorId = codes[0].get("creator_rally_id");
-    console.log("creatorId", creatorId);
     return res
       .status(200)
       .json({ redirect_url: `/${creatorId}/earn`, valid_code: true });
