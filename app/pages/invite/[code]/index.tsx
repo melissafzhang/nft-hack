2b40b89ce71056ab6d87d04313ee243e6e77cf48
Melissa Zhang <melz63017@gmail.com>
2021-03-20 18:02:41 -0700

    Add comment

--- app/pages/invite/[code]/index.tsx
+++ app/pages/invite/[code]/index.tsx
@@ -16,7 +16,13 @@ export async function getServerSideProps(context) {
   const data = await response.json();
   const cookies = new Cookies(context.req, context.res);
 
+  // Weird things may happen if the user clicks on multiple referral
+  // links. e.g. User clicks on a referral link for creator A then navigates
+  // away. User then clicks another referral link for creator B. If user navigates
+  // back to creator A's page directly and subscribes then the wrong referral code
+  // may be applied
   if (data.valid_code) cookies.set("referral_code", code);
+
   return {
     redirect: {
       permanent: true,
